c16ba0031269145ba221141e73670a8963577017
Developer <dev@musicapp.com>
2026-04-17 12:31:50 +0530

    latest commit 7

--- backend/app/app.js
+++ backend/app/app.js
@@ -6,10 +6,21 @@ const musicRoutes = require('../routes/music.routes.js')
 const app = express();
 app.use(cookieparser())
 app.use(express.json());
+const allowedOrigins = [
+  'https://musicplayer-chi-woad.vercel.app',
+  'http://localhost:3000'
+];
+
 app.use(cors({
-  origin: "https://musicplayer-chi-woad.vercel.app/",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
-}))
+}));
 
 app.use('/api/auth',authRoutes)
 app.use('/api/music',musicRoutes)
