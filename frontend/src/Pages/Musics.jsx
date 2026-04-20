66ec39977816b32f549b84e0be64244291cdb4f6
Developer <dev@musicapp.com>
2026-04-20 16:01:33 +0530

    like button fixed

--- frontend/src/Pages/Musics.jsx
+++ frontend/src/Pages/Musics.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "../assets/Musics.css";
+import {love} from "../assets/love.png"
 
 function Musics() {
 
@@ -169,7 +170,7 @@ function Musics() {
 
 
             <img
-              src= "../src/assets/love.png"
+              src= {love}
               alt=""
               width="30px"
               style={{ backgroundColor: item.likedBy?.includes(userId) ? "red" : "white", borderRadius: '20px' }}
